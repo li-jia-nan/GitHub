40288cc9b2a488b728b00b31f3044528d63b5027
栗嘉男 <574980606@qq.com>
2022-06-03 12:22:52 +0800

    init

---
+++ pages/api/hello.ts
@@ -0,0 +1,11 @@
+import type { NextApiHandler, NextApiRequest, NextApiResponse } from 'next';
+
+interface Data {
+  name: string;
+}
+
+const handler: NextApiHandler = (req: NextApiRequest, res: NextApiResponse<Data>) => {
+  res.status(200).json({ name: 'John Doe' });
+};
+
+export default handler;
