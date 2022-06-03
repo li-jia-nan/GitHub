709940248f68f538b5d05338b24be6d8dfe7aa27
栗嘉男 <574980606@qq.com>
2022-06-04 00:20:22 +0800

    fix

---
+++ config.ts
@@ -0,0 +1,4 @@
+export const github = {
+  ClientID: 'ba73feb9e2f47f18e8ff',
+  ClientSecrets: 'f4d8e951f205001bfc3c44643f7bda8ae29e336d',
+};
--- server.js
+++ server.js
@@ -1,19 +1,42 @@
 const Koa = require('koa');
+const Router = require('koa-router');
 const next = require('next');
+const session = require('koa-session');
+const Redis = require('ioredis');
+const RedisSessionStore = require('./src/server/session-store');
 
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
+const redis = new Redis();
+
+const SESSION_CONFIG = {
+  key: 'jid',
+  store: new RedisSessionStore(redis),
+};
+
 app.prepare().then(() => {
   const server = new Koa();
+  const router = new Router();
+  server.keys = ['aaa'];
+  server.use(session(SESSION_CONFIG, server));
+  router.get('/api/user', async (ctx, next) => {
+    ctx.session.user = {
+      name: 'sdsd',
+      age: 18,
+    };
+    ctx.body = 'sdsffdf';
+    await next();
+  });
   server.use(async (ctx, next) => {
+    // ctx.cookies.set('id', 'userid:xxxxx')
+    ctx.req.session = ctx.session;
     await handle(ctx.req, ctx.res);
     ctx.respond = false;
   });
+  server.use(router.routes());
   server.listen(3000, () => {
-    console.log('====================================');
     console.log('koa listen on 3000');
-    console.log('====================================');
   });
 });
